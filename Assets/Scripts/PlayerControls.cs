157eba1eca8bcf5aed8ee29102ef7d7cbcaed8dd
Sergey Zimin <s@zimin.me>
2022-04-02 23:51:14 +0300

    Added movement control

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] float controlSpeed = 30f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 10f;
+
+    void Update()
+    {
+        float xThrow = Input.GetAxis("Horizontal");        
+        float yThrow = Input.GetAxis("Vertical");
+
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float xPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(xPos, -xRange, xRange);
+
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float yPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(yPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+        
+    }
+}
